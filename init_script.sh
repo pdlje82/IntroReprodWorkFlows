fc9be94f69aacf23bc53e55e72d3cd8715374a77
Jan Engelke <jan.engelke@gmx.net>
2023-09-27 17:32:02 +0200

    init script update

--- init_script.sh
+++ init_script.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/pdlje82/IntroReprodWorkFlows.git
+
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 bash Miniconda3-latest-Linux-x86_64.sh -u -b -p /workspace/miniconda3
@@ -7,7 +9,7 @@ conda init bash
 source ~/.bashrc
 
 
-!conda clean --all
+conda clean --all
 conda create --name IntroReproducibleWF python=3.8 mlflow jupyter pandas matplotlib requests -c conda-forge
 
 conda activate IntroReproducibleWF
@@ -15,4 +17,3 @@ conda activate IntroReproducibleWF
 pip install wandb==0.10.29
 wandb login
 
-git clone https://github.com/pdlje82/IntroReprodWorkFlows.git
