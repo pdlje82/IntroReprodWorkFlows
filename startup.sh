7a472e466d2780a1853aedecfad11d9fdf427a0e
Jan Engelke <jan.engelke@gmx.net>
2023-09-29 11:50:06 +0200

    changes

--- startup.sh
+++ startup.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-# Function to add content to .bashrc if it doesn't already exist
-add_to_bashrc() {
-    local content="$1"
-    if ! grep -qF -- "$content" ~/.bashrc; then
-        echo "$content" >> ~/.bashrc
-    fi
-}
-
 # Update package lists and install nano
 apt update && apt install -y nano
 
-# Add Git branch display function and modify PS1 variable
-add_to_bashrc 'parse_git_branch() { git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/ (\1)/"; }'
-add_to_bashrc 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\$ "'
+# Function to display the Git branch
+echo 'parse_git_branch() {
+   git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/ (\1)/"
+}' >> ~/.bashrc
 
-# Adjust PATH
-add_to_bashrc 'export PATH="/workspace/miniforge3/bin:$PATH"'
+# Modify the PS1 variable
+echo 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\$ "' >> ~/.bashrc
+
+# Adjust PATH and initialize conda
+echo 'export PATH="/workspace/miniforge3/bin:$PATH"' >> ~/.bashrc
+
+# Source .bashrc to apply changes so far
+source ~/.bashrc
 
 # Initialize conda
-add_to_bashrc 'conda init bash || true'
+conda init bash
+
+# Source .bashrc again to have conda changes take effect
 source ~/.bashrc
 
 # Initialize mamba
-add_to_bashrc 'mamba init bash || true'
+mamba init bash
+
+# Source .bashrc again to have mamba changes take effect
 source ~/.bashrc
+
+mamba activate IntroReproducibleWF
 
 # Create symlink to make 'conda' command use 'mamba'
 CONDA_PATH="/workspace/miniforge3/bin/conda"
